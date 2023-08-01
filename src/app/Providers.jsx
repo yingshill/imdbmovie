88b8b61f4368e5dce5be07f2c4892712b0e347c2
Yingshi Liu <serendipper.liu@gmail.com>
2023-08-01 12:14:12 -0700

    Add ABOUT page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -6,8 +6,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({children}) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-        <div className="dark: bg-gray-700 dark: text-gray-200 light: text-gray-700 light: bg-gray-200
-            transition-colors duration-300 min-h-screen select-none" >
+        <div className="dark:bg-gray-700 dark:text-gray-200 transition-colors delay-150 bg-white text-gray-700 min-h-screen selection:bg-cyan-500 selection:text-white" >
             {children}
         </div>
         
