231d52686232da3d590eebade82840167c2d7518
Yingshi Liu <serendipper.liu@gmail.com>
2023-07-31 18:36:15 -0700

    Added About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+    </div>
+  )
+}
